a3337c05ef3deac95b76c1b8448a1b182baed0c0
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-18 16:23:16 +0200

    finish scrap

--- app/models/criterium.rb
+++ app/models/criterium.rb
@@ -1,4 +1,7 @@
 class Criterium < ApplicationRecord
+  PLATFORMS = ["nfx", "dnp", "itu", "ocs", "prv", "cpd"]
+  PLATFORMS_FULL_NAME = ["netflix", "disneyplus", "itunes", "go.ocs", "amazon", "primevideo", "canalplus"]
+
   belongs_to :user
 
   before_validation :clean_empty_platforms
