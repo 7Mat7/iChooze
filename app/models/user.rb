8339b665e4f2245b5192f9a64ca8ce1b59483f6a
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-22 15:29:39 +0200

    signup redirect to home

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+  after_create :create_criteria
+
   has_many :vues
   has_many :movies, through: :vues
   has_one :criterium
+
+  def create_criteria
+    @criterium = Criterium.create(user: self)
+  end
 end
